03217569ee5b34ba073b82dbec266fd81bb5c4a5
rigelHadushiDev <rigelhadushi4@gmail.com>
2025-04-08 00:24:51 +0200

    pulling from dev

--- src/main/java/com/example/job_application_eval/service/UserService.java
+++ src/main/java/com/example/job_application_eval/service/UserService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface UserService {
+
     List<UserEntity> allUsers();
 
     UserEntity getCurrentUserEntity();
