b52a5ca8b1668b104d36b3b06eaa4e825d52a2fd
rigelHadushiDev <rigelhadushi4@gmail.com>
2025-11-10 23:56:55 +0100

    dockerized back end + db. Left to do unit and integration tests.

--- src/main/java/com/example/job_application_eval/config/EmailConfiguration.java
+++ src/main/java/com/example/job_application_eval/config/EmailConfiguration.java
@@ -7,8 +7,6 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
-
-
 @Configuration
 public class EmailConfiguration {
 
